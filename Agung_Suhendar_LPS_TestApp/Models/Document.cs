88757184284c67f4237b982dfa63490a50109571
Agungsuhendar <agung.suhendarkp@gmail.com>
2024-10-28 13:12:07 +0700

    Update

--- Agung_Suhendar_LPS_TestApp/Models/Document.cs
+++ Agung_Suhendar_LPS_TestApp/Models/Document.cs
@@ -2,7 +2,7 @@
 {
     public class Document
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public byte[] Content { get; set; }
         public DateTime UploadedDate { get; set; }
